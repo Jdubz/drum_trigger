7d134af9e92f3dacdfe81d922ab1bc54717377ac
Josh Wentworth <contact@joshwentworth.com>
2019-09-20 00:40:31 -0700

    led indicators buggy

--- piezo_sketch/piezo_sketch.ino
+++ piezo_sketch/piezo_sketch.ino
@@ -1,12 +1,19 @@
 #include <MIDI.h>
+#include <Adafruit_NeoPixel.h>
+
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 const int LEDPIN = 2;
+const int LEDS = 10;
+
 const int MIDI_CHANNEL = 10;
 const int NUMSENSORS = 20;
 // const int NUMSENSORS = 1;
 const float upthreshold = 30.0;
 const int debounce = 30;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, LEDPIN, NEO_GRB + NEO_KHZ800);
+const int ledMap[NUMSENSORS] = {10,10,10,10,10,10,10,10,10,0,1,2,3, 4,5,6,7,8,9};
 
 int sensors[NUMSENSORS] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A15,A16,A17,A18,A19,A20};
 // int sensors[NUMSENSORS] = {A8};
@@ -21,8 +28,16 @@ void findFloor(int sensor, float amp) {
   }
 }
 
+void renderStrip() {
+  strip.show();
+  for (int led = 0; led < LEDS; led++) {
+    strip.setPixelColor(led, 0, 0, 0);
+  }
+}
+
 void setup()
 {
+  
   for (int sensor = 0; sensor < NUMSENSORS; sensor++) {
     isUp[sensor] = false;
     floors[sensor] = 0.0;
@@ -34,17 +49,31 @@ void setup()
   
    Serial.begin(9600); 
 //   MIDI.begin(MIDI_CHANNEL_OMNI);
+  strip.begin();
+  for (int led = 0; led < LEDS; led++) {
+    strip.setPixelColor(led, 100, 100, 100);
+  }
+  strip.show();
+  delay(3000);
+
 }
 
 void loop()
 {
   for (int sensor = 0; sensor < NUMSENSORS; sensor++) {
     float amp = analogRead(sensors[sensor]);
-    findFloor(sensor, amp);
+    // findFloor(sensor, amp);
 //    int noteNumber = 60 + sensor;
     if (amp > upthreshold) {
       Serial.println(String(sensor) + " ---- " + String(amp));
+      int velocity = (amp / 1024.0) * 255.0;
+    // Serial.println(velocity);
+      strip.setPixelColor(ledMap[sensor], velocity * 10, velocity * 10, velocity *10);
     }
+
+    
+  }
+    
 //    if (amp > upthreshold && !isUp[sensor]) {
 //      float velocity = (amp / 1024.0) * 127.0;
 //      // Serial.println(amp);
@@ -56,5 +85,5 @@ void loop()
 //      Serial.println(String(noteNumber) + " : " + String(amp) + " off");
 //      sensors[sensor] = false;
 //    }
-  }
+  renderStrip();
 }
